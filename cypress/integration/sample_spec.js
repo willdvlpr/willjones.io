29e0911c01dcf9443581e76bb8b8a5102d01b00b
Will Jones <willjones484@gmail.com>
2023-04-23 11:12:58 +1000

    chore: remove cypress, update package

--- cypress/integration/sample_spec.js
+++
@@ -1,7 +0,0 @@
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    cy.visit("https://www.willjones.io");
-    cy.contains("Software Engineer.");
-    cy.contains("contact").click();
-  });
-});
