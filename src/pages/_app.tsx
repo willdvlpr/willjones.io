2a80ef9d0d9117823a9b26282c538d2b0e3e1bc8
Will Jones <willjones484@gmail.com>
2023-06-04 14:32:48 +1000

    testing branch protection

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,12 @@ import React from "react";
 import { Analytics } from "@vercel/analytics/react";
 import "../styles/global.css";
 
-interface Props {
+interface AppProps {
   Component: any;
   pageProps: any;
 }
 
-const App = ({ Component, pageProps }: Props) => {
+const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Component {...pageProps} />
